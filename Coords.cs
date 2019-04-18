1352eabfceb14aa5550fd2ec77815da37dfa4c0e
Ryan White <WHITER23@my.erau.edu>
2019-04-18 10:01:34 -0400

    test

---
+++ Coords.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    public struct Coords
+    {
+        private readonly double y;
+
+        public double X
+        {
+            get;
+            set; 
+        }
+
+        public double Y
+        {
+            get => X;
+            set => X = value;
+        }
+
+        // Sets a random value to coords
+        public Coords(double x, double y)
+        {
+            X = x;
+            this.y = y;
+        }
+
+        public Coords(Random random)
+        {
+            X = random.NextDouble();
+            y = random.NextDouble();
+        }
+
+        // Calculates the hypotenuse 
+        public double Hypotenuse()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(y, 2));
+        }
+    }
+}
---
+++ MonteCarloTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    public class MonteCarloTests
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            var coords = new Coords[arrayLength];
+            var random = new Random();
+            var counter = 0;
+
+            for (var i = 0; i < arrayLength; i++)
+            {
+                coords[i] = new Coords(random);
+                if (coords[i].Hypotenuse() <= 1) counter++;
+            }
+
+            // Divide coordinates overlapping circle by the number of elements then multiply
+            // value by four.
+            var avg = counter / (double) coords.Length * 4;
+          
+            Console.WriteLine($"Points overlapping circle - {counter}");
+            
+            Console.WriteLine($"generated = {avg}, actual = {Math.PI}, difference = {Math.Abs(Math.PI - avg)}");
+            Console.ReadKey();
+        }
+    }
+}
